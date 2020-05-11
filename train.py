1a6c6ff61c7b24424453654acb07bf28a36fb548
nadavSellam <Nadavonyy@gmail.com>
2020-05-11 17:05:52 +0300

    fixed test and train

--- train.py
+++ train.py
@@ -3,14 +3,13 @@ import time
 import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
-from torch import nn
-from torch.nn import functional
 from torch.utils.data import DataLoader
 
 from mri_data import SliceData
 from model import SubSamplingModel
 import utils
 from SSIM import ssim
+import numpy as np
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -44,11 +43,10 @@ def data_transform(k_space, mask, target, attrs, f_name, slice):
     return cropped_k_space, cropped_image, slice, target
 
 
-def train_model(network, train_data, number_of_epochs):
+def train_model(network, train_data, number_of_epochs,learning_rate=0.05,learning_rate_decay = True):
     network = network.to(device)
     # define loos and optimizer
-    optimizer = optim.Adam(network.parameters(), 0.03)
-    print(network.parameters())
+    optimizer = optim.Adam(network.parameters(),learning_rate)
     print('Starting Training')
     start_time = time.time()
     print("train_data len is: " + str(len(train_data)))
@@ -76,27 +74,35 @@ def train_model(network, train_data, number_of_epochs):
             running_loss += loss.item()
         print('running_loss(l1) = ' + str(running_loss))
         print('Epoch time: ' + str(time.time() - running_time))
+        # the decaying of the learning rate the closer
+        if epoch_number % 15 == 14 and learning_rate_decay:
+            learning_rate *= 0.5
+            optimizer = optim.Adam(network.parameters(), learning_rate)
     print('Overall time: ' + str(time.time() - start_time))
     print('Finished Training')
 
 
-def test_model(model, data):
+def test_model(model, data,limit=10,seed=0):
+    # the limit makes sure the amount of picture will not be too large
     print('Starting Testing')
+    # make sure to pick the same pictures every time
+    np.random.seed(seed)
     i = 0
     for cropped_k_space, cropped_image, slice, target in data:
-        if slice[0] > 10:
+        if np.random.randn() >= 0.8:
             i += 1
-            if i % 10 == 0:
-                cropped_k_space = cropped_k_space.to(device)
-                plt.figure()
-                image = model(cropped_k_space.unsqueeze(1)).detach().cpu().squeeze()
-                plt.imshow(image[0], cmap='gray')
-                plt.title('the model output')
-                plt.figure()
-                plt.imshow(target[0].squeeze().detach().cpu(), cmap='gray')
-                plt.title('target image')
-                plt.show()
-                a = 5
+            cropped_k_space = cropped_k_space.to(device)
+            plt.figure()
+            image = model(cropped_k_space.unsqueeze(1)).detach().cpu().squeeze()
+            plt.imshow(image[0], cmap='gray')
+            plt.title('the model output')
+            plt.figure()
+            plt.imshow(target[0].squeeze().detach().cpu(), cmap='gray')
+            plt.title('target image')
+            plt.show()
+            if i == limit:
+                break
+
 
 
 def load_data():
@@ -121,23 +127,26 @@ def main():
     data = load_data()
     model = SubSamplingModel(decimation_rate=4, resolution=320, trajectory_learning=True)
     model = model.to(device)
-
-    # Print summary of the model
-    # summary(model, (1, 320, 320, 2))
-
     if len(data) <= 0:
         print('Dataloader failed')
         return
     else:
-        train_model(model, data, 50)
-        # test_model(model, data)
+        train_model(model, data, 1)
+        test_model(model, data)
+
     # path = './network_30_epochs.pth'
     # model.load_state_dict(torch.load(path))
 
-    # train(train_loader, network, 20)
-    path = './network_50_epochs.pth'
+    path = './network_1_epochs.pth'
     torch.save(model.state_dict(), path)
 
 
 if __name__ == '__main__':
-    main()
+    # path = './network_5_epochs.pth'
+    # model = SubSamplingModel(decimation_rate=4, resolution=320, trajectory_learning=True)
+    # model = model.to(device)
+    # model.load_state_dict(torch.load(path))
+    # model.eval()
+    # data = load_data()
+    # test_model(model,data)
+   main()
