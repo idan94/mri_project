26e848e8acc97a8d0e22ec887d2834224a6db88b
nadavSellam <Nadavonyy@gmail.com>
2020-06-25 18:24:57 +0300

    working demo

--- test grid sample.py
+++ test grid sample.py
@@ -63,9 +63,9 @@ def main():
         sampling_vector = torch.tensor(sampling_vector).float()
         sampling_vector = sampling_vector - 0.5 * k_space.shape[1]
         sampling_vector = sampling_vector.reshape(-1, 2)
+        sampling_vector = sampling_vector.expand(k_space.shape[0],-1,-1)
         images = sample_vector(k_space,sampling_vector)
         break
-
 
     for i in range(images.shape[0]):
         show(ifft2(images[i]))
